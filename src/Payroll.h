b9499b104bcb0339675d43b13e587cd993d363d2
Carla Z <carla.zacarias@g.austincc.edu>
2017-09-20 18:32:39 -0500

    uploaded function prototypes

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,7 +1,22 @@
 #ifndef _PAYROLL_H
+
+
+
+
+
+
 #define _PAYROLL_H
 
-
-
+string getName();
+char getCat();
+double getOverTime(double);
+double regHours(char);
+bool OT(double);
+double payRateSalary(char);
+double payOrSalary(double, char);
+double getFicaSSN(double);
+double getFicaMED(double);
+double getFIT(double);
+double getNetPay(double, double, double, double);
 
 #endif
